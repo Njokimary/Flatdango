c421153fe60b2af315c96796c64b4b813c14da80
MARY NJOKI <marynjoki738@gmail.com>
2023-07-29 15:42:34 +0300

    Function to handle Buy Ticket button click

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
+// Function to make a GET request to the JSON DB server
+function fetchMovieData(url) {
+    return fetch(url)
+        .then(response => response.json());
+}
+
+// Function to handle "Buy Ticket" button click
+function buyTicket(movieId, availableTickets) {
+    if (availableTickets > 0) {
+        // we'll just display a message
+        alert("Ticket purchased successfully!");
+        // Simulate ticket purchase by decrementing tickets_sold 
+        const movieCard = document.getElementById(`movie-${movieId}`);
+        const ticketElement = movieCard.querySelector(".movie-available-tickets");
+        ticketElement.textContent = `Available Tickets: ${availableTickets - 1}`;
+    } else {
+        alert("Sorry, this movie is sold out.");
+    }
+}
+
 document.addEventListener("DOMContentLoaded", function () {
     const filmsList = document.getElementById("films");
     const movieDetails = document.getElementById("movie-details");
-    const baseUrl = "http://localhost:3000"; 
-
-    // Function to make a GET request to the JSON DB server
-    function fetchMovieData(url) {
-        return fetch(url)
-        .then(response => response.json());
-    }
+    const baseUrl = "http://localhost:3000";
 
     // Function to populate the movie menu with cards
     function populateMovieMenu(movies) {
@@ -18,8 +32,14 @@ document.addEventListener("DOMContentLoaded", function () {
             card.innerHTML = `
                 <img class="movie-image" src="${movie.poster}" alt="${movie.title} Poster">
                 <h2 class="movie-title">${movie.title}</h2>
+                <div class="movie-details-container">
+                    <p class="movie-runtime">Runtime: ${movie.runtime} minutes</p>
+                    <p class="movie-showtime">Showtime: ${movie.showtime}</p>
+                    <p class="movie-available-tickets">Available Tickets: ${movie.capacity - movie.tickets_sold}</p>
+                    <p class="movie-description">${movie.description}</p>
+                    <button class="buy-ticket-btn" onclick="buyTicket(${movie.id}, ${movie.capacity - movie.tickets_sold})">Buy Ticket</button>
+                </div>
             `;
-            card.addEventListener("click", () => showMovieDetails(movie.id));
             filmsList.appendChild(card);
         });
     }
@@ -28,39 +48,22 @@ document.addEventListener("DOMContentLoaded", function () {
     function showMovieDetails(movieId) {
         const movieUrl = `${baseUrl}/films/${movieId}`;
         fetchMovieData(movieUrl)
-        .then(movie => {
-            const availableTickets = movie.capacity - movie.tickets_sold;
-            movieDetails.innerHTML = `
-                <img src="${movie.poster}" alt="${movie.title} Poster">
-                <h2>${movie.title}</h2>
-                <p>Runtime: ${movie.runtime} minutes</p>
-                <p>Showtime: ${movie.showtime}</p>
-                <p>Available Tickets: ${availableTickets}</p>
-                <p>Description: ${movie.description}</p>
-                <button onclick="buyTicket(${movieId}, ${availableTickets})">Buy Ticket</button>
-            `;
-        });
-    }
-
-    // Function to handle "Buy Ticket" button click
-    function buyTicket(movieId, availableTickets) {
-        if (availableTickets > 0) {
-            // we'll just display a message
-            alert("Ticket purchased successfully!");
-            // Update the available tickets on the frontend
-            const ticketElement = movieDetails.querySelector("p:last-child");
-            ticketElement.textContent = `Available Tickets: ${availableTickets - 1}`;
-        } else {
-            alert("Sorry, this movie is sold out.");
-        }
+            .then(movie => {
+                const movieCard = document.getElementById(`movie-${movieId}`);
+                movieCard.querySelector(".movie-description").textContent = movie.description;
+                const availableTickets = movie.capacity - movie.tickets_sold;
+                if (availableTickets > 0) {
+                    movieCard.querySelector(".buy-ticket-btn").style.display = "block";
+                } else {
+                    movieCard.querySelector(".buy-ticket-btn").style.display = "none";
+                }
+            });
     }
 
     // Function to initialize the app
     function init() {
         const moviesUrl = `${baseUrl}/films`;
         fetchMovieData(moviesUrl).then(populateMovieMenu);
-        // Show details of the first movie initially
-        showMovieDetails(1); 
     }
 
     init();
